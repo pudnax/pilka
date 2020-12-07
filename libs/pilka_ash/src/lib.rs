ca45d143bef06cd6e7652b25704dc62cd235089a
Alex Komissarov <k.a.komissar@gmail.com>
2020-12-07 14:13:44 +0500

    Move `Instance` and `entry` into external crate.

--- libs/pilka_ash/src/lib.rs
+++ libs/pilka_ash/src/lib.rs
@@ -1,7 +1,190 @@
-pub mod ash {
-    pub use ash::*;
-}
+use std::{
+    borrow::Cow,
+    ffi::{CStr, CString},
+};
 
 pub mod ash_window {
     pub use ash_window::*;
+}
+pub mod ash {
+    pub use ash::*;
+
+    pub use crate::ash::{
+        extensions::{
+            ext::DebugUtils,
+            khr::{Surface, Swapchain},
+        },
+        prelude::VkResult,
+        version::{DeviceV1_0, EntryV1_0, InstanceV1_0},
+        vk,
+    };
+
+    use std::{
+        borrow::Cow,
+        ffi::{CStr, CString},
+    };
+
+    /// The entry point for vulkan application.
+    pub struct VkInstance {
+        pub entry: ash::Entry,
+        pub instance: ash::Instance,
+        _dbg_loader: Option<ash::extensions::ext::DebugUtils>,
+        _dbg_callbk: Option<vk::DebugUtilsMessengerEXT>,
+    }
+
+    impl VkInstance {
+        pub fn new(
+            app_name: String,
+            engine_name: String,
+            layers: &[&CStr],
+            extensions: &[&CStr],
+        ) -> Self {
+            let entry = ash::Entry::new().unwrap();
+
+            // Enumerate available vulkan API version and set 1.0.0 otherwise.
+            let version = match entry.try_enumerate_instance_version().unwrap() {
+                Some(version) => version,
+                None => vk::make_version(2, 0, 0),
+            };
+
+            // Find approciate validation layers from available.
+            let available_layers = entry.enumerate_instance_layer_properties().unwrap();
+            let validation_layers = layers
+                .iter()
+                .map(|s| unsafe { CStr::from_ptr(s.as_ptr() as *const i8) })
+                .filter_map(|lyr| {
+                    available_layers
+                        .iter()
+                        .find(|x| unsafe { CStr::from_ptr(x.layer_name.as_ptr()) } == lyr)
+                        .map(|_| lyr.as_ptr())
+                        .or_else(|| {
+                            println!(
+                            "Unable to find layer: {}, have you installed the Vulkan SDK.unwrap()",
+                            lyr.to_string_lossy()
+                        );
+                            None
+                        })
+                })
+                .collect::<Vec<_>>();
+
+            // Find approciate extensions from available.
+            let available_exts = entry.enumerate_instance_extension_properties().unwrap();
+            let extensions = extensions
+            .iter()
+            .map(|s| unsafe { CStr::from_ptr(s.as_ptr() as *const i8) })
+            .filter_map(|ext| {
+                available_exts
+                    .iter()
+                    .find(|x| unsafe { CStr::from_ptr(x.extension_name.as_ptr()) } == ext)
+                    .map(|_| ext.as_ptr())
+                    .or_else(|| {
+                        println!(
+                            "Unable to find extension: {}, have you installed the Vulkan SDK.unwrap()",
+                            ext.to_string_lossy()
+                        );
+                        None
+                    })
+            })
+            .collect::<Vec<_>>();
+
+            let app_info = vk::ApplicationInfo::builder()
+                .application_name(unsafe { CStr::from_ptr(app_name.as_ptr() as *const i8) })
+                .engine_name(unsafe { CStr::from_ptr(engine_name.as_ptr() as *const i8) })
+                .engine_version(vk::make_version(1, 1, 0))
+                .api_version(version);
+
+            let instance_info = vk::InstanceCreateInfo::builder()
+                .application_info(&app_info)
+                .enabled_layer_names(&validation_layers)
+                .enabled_extension_names(&extensions);
+
+            let instance = unsafe { entry.create_instance(&instance_info, None) }.unwrap();
+
+            let (_dbg_loader, _dbg_callbk) = if cfg!(debug_assertions) {
+                let dbg_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+                    .message_severity(
+                        vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+                            | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING, // | vk::DebugUtilsMessageSeverityFlagsEXT::INFO,
+                    )
+                    .message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
+                    .pfn_user_callback(Some(vulkan_debug_callback));
+                let dbg_loader = DebugUtils::new(&entry, &instance);
+                let dbg_callbk = unsafe {
+                    dbg_loader
+                        .create_debug_utils_messenger(&dbg_info, None)
+                        .unwrap()
+                };
+                (Some(dbg_loader), Some(dbg_callbk))
+            } else {
+                (None, None)
+            };
+
+            Self {
+                entry,
+                instance,
+                _dbg_loader,
+                _dbg_callbk,
+            }
+        }
+    }
+
+    impl Drop for VkInstance {
+        fn drop(&mut self) {
+            if let Some(ref _dbg_loader) = self._dbg_loader {
+                if let Some(_dbg_callbk) = self._dbg_callbk {
+                    unsafe { _dbg_loader.destroy_debug_utils_messenger(_dbg_callbk, None) };
+                }
+            }
+            unsafe { self.instance.destroy_instance(None) };
+        }
+    }
+
+    unsafe extern "system" fn vulkan_debug_callback(
+        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+        message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+        p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+        _user_data: *mut std::os::raw::c_void,
+    ) -> vk::Bool32 {
+        let callback_data = &*p_callback_data;
+        let message_id_number: i32 = callback_data.message_id_number as i32;
+
+        let message_id_name = if callback_data.p_message_id_name.is_null() {
+            Cow::from("")
+        } else {
+            CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy()
+        };
+
+        let message = if callback_data.p_message.is_null() {
+            Cow::from("")
+        } else {
+            CStr::from_ptr(callback_data.p_message).to_string_lossy()
+        };
+
+        println!(
+            "{:?}:\n{:?} [{} ({})] : {}\n",
+            message_severity, message_type, message_id_name, message_id_number, message,
+        );
+
+        vk::FALSE
+    }
+}
+
+unsafe fn extract_entries(sample: Vec<String>, entries: Vec<String>) -> Vec<*const i8> {
+    entries
+        .iter()
+        .map(|s| unsafe { CStr::from_ptr(s.as_ptr() as *const i8) })
+        .filter_map(|lyr| {
+            sample
+                .iter()
+                .find(|x| unsafe { CStr::from_ptr(x.as_ptr() as *const i8) } == lyr)
+                .map(|_| lyr.as_ptr())
+                .or_else(|| {
+                    println!(
+                        "Unable to find layer: {}, have you installed the Vulkan SDK.unwrap()",
+                        lyr.to_string_lossy()
+                    );
+                    None
+                })
+        })
+        .collect::<Vec<_>>()
 }
--- src/main.rs
+++ src/main.rs
@@ -68,152 +68,60 @@ static EXTS: SyncLazy<Vec<&'static CStr>> = SyncLazy::new(|| {
     exts
 });
 
-unsafe extern "system" fn vulkan_debug_callback(
-    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
-    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
-    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
-    _user_data: *mut std::os::raw::c_void,
-) -> vk::Bool32 {
-    let callback_data = &*p_callback_data;
-    let message_id_number: i32 = callback_data.message_id_number as i32;
-
-    let message_id_name = if callback_data.p_message_id_name.is_null() {
-        Cow::from("")
-    } else {
-        CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy()
-    };
-
-    let message = if callback_data.p_message.is_null() {
-        Cow::from("")
-    } else {
-        CStr::from_ptr(callback_data.p_message).to_string_lossy()
-    };
-
-    println!(
-        "{:?}:\n{:?} [{} ({})] : {}\n",
-        message_severity, message_type, message_id_name, message_id_number, message,
-    );
-
-    vk::FALSE
-}
-
 fn main() -> Result<()> {
     // Initialize error hook.
     color_eyre::install()?;
-
-    let engine_name = CStr::from_bytes_with_nul(b"Ruchka Engine\0")?;
-    let app_name = CStr::from_bytes_with_nul(b"Pilka\0")?;
+    let app_name = "app2".to_string();
+    let eng_name2 = "eng".to_string();
 
     let mut event_loop = winit::event_loop::EventLoop::new();
     let window = winit::window::Window::new(&event_loop)?;
-    window.set_title(&app_name.to_string_lossy());
+    window.set_title(&app_name);
     let surface_extensions = ash_window::enumerate_required_extensions(&window)?;
 
-    let entry = ash::Entry::new()?;
-
-    // Enumerate available vulkan API version and set 1.0.0 otherwise.
-    let version = match entry.try_enumerate_instance_version()? {
-        Some(version) => version,
-        None => vk::make_version(1, 0, 0),
-    };
-
-    // Find approciate validation layers from available.
-    let available_layers = entry.enumerate_instance_layer_properties()?;
-    let validation_layers = LAYERS
-        .iter()
-        .filter_map(|&lyr| {
-            available_layers
-                .iter()
-                .find(|x| unsafe { CStr::from_ptr(x.layer_name.as_ptr()) } == lyr)
-                .map(|_| lyr.as_ptr())
-                .or_else(|| {
-                    println!(
-                        "Unable to find layer: {}, have you installed the Vulkan SDK?",
-                        lyr.to_string_lossy()
-                    );
-                    None
-                })
-        })
+    let extensions = surface_extensions
+        .into_iter()
+        .chain(EXTS.iter().cloned())
         .collect::<Vec<_>>();
-
-    // Find approciate extensions from available.
-    let exist_exts = entry.enumerate_instance_extension_properties()?;
-    SyncLazy::force(&EXTS);
-    let extensions = EXTS
-        .iter()
-        .filter_map(|&ext| {
-            exist_exts
-                .iter()
-                .find(|x| unsafe { CStr::from_ptr(x.extension_name.as_ptr()) } == ext)
-                .map(|_| ext.as_ptr())
-                .or_else(|| {
-                    println!(
-                        "Unable to find extension: {}, have you installed the Vulkan SDK?",
-                        ext.to_string_lossy()
-                    );
-                    None
-                })
-        })
-        .chain(surface_extensions.iter().map(|s| s.as_ptr()))
-        .collect::<Vec<_>>();
-
-    let app_info = vk::ApplicationInfo::builder()
-        .application_name(app_name)
-        .engine_name(engine_name)
-        .engine_version(vk::make_version(0, 1, 0))
-        .api_version(version);
-
-    let instance_info = vk::InstanceCreateInfo::builder()
-        .application_info(&app_info)
-        .enabled_layer_names(&validation_layers)
-        .enabled_extension_names(&extensions);
-
-    let instance = unsafe { entry.create_instance(&instance_info, None) }?;
-
-    let (_dbg_loader, _dbg_callbk) = if cfg!(debug_assertions) {
-        let dbg_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
-            .message_severity(
-                vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
-                    | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING, // | vk::DebugUtilsMessageSeverityFlagsEXT::INFO,
-            )
-            .message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
-            .pfn_user_callback(Some(vulkan_debug_callback));
-        let dbg_loader = DebugUtils::new(&entry, &instance);
-        let dbg_callbk = unsafe {
-            dbg_loader
-                .create_debug_utils_messenger(&dbg_info, None)
-                .unwrap()
-        };
-        (Some(dbg_loader), Some(dbg_callbk))
-    } else {
-        (None, None)
-    };
+    let instance = ash::VkInstance::new(
+        app_name,
+        eng_name2,
+        LAYERS.as_slice(),
+        extensions.as_slice(),
+    );
 
     // Make surface and surface loader.
-    let surface = unsafe { ash_window::create_surface(&entry, &instance, &window, None) }?;
-    let surface_loader = Surface::new(&entry, &instance);
+    let surface =
+        unsafe { ash_window::create_surface(&instance.entry, &instance.instance, &window, None) }?;
+    let surface_loader = Surface::new(&instance.entry, &instance.instance);
 
     // Acuire all availble device for this machine.
-    let phys_devices = unsafe { instance.enumerate_physical_devices() }?;
+    let phys_devices = unsafe { instance.instance.enumerate_physical_devices() }?;
 
     // Choose physical device assuming that we want to choose discrete GPU.
     let (physical_device, _device_properties, device_features) = {
         let mut chosen = Err(vk::Result::ERROR_INITIALIZATION_FAILED);
         for p in phys_devices {
-            let properties = unsafe { instance.get_physical_device_properties(p) };
-            let features = unsafe { instance.get_physical_device_features(p) };
+            let properties = unsafe { instance.instance.get_physical_device_properties(p) };
+            let features = unsafe { instance.instance.get_physical_device_features(p) };
             if properties.device_type == vk::PhysicalDeviceType::DISCRETE_GPU {
                 chosen = Ok((p, properties, features));
             }
         }
         chosen
     }?;
-    let device_memory_properties =
-        unsafe { instance.get_physical_device_memory_properties(physical_device) };
+    let device_memory_properties = unsafe {
+        instance
+            .instance
+            .get_physical_device_memory_properties(physical_device)
+    };
 
     // Choose graphics and transfer queue familities.
-    let queuefamilyproperties =
-        unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
+    let queuefamilyproperties = unsafe {
+        instance
+            .instance
+            .get_physical_device_queue_family_properties(physical_device)
+    };
     let mut found_graphics_q_index = None;
     let mut found_transfer_q_index = None;
     for (index, qfam) in queuefamilyproperties.iter().enumerate() {
@@ -252,11 +160,15 @@ fn main() -> Result<()> {
     let device_extension_name_pointers: Vec<*const i8> = vec![Swapchain::name().as_ptr()];
 
     let device_info = vk::DeviceCreateInfo::builder()
-        .enabled_layer_names(&validation_layers)
+        // .enabled_layer_names(&validation_layers)
         .enabled_extension_names(&device_extension_name_pointers)
         .enabled_features(&device_features)
         .queue_create_infos(&queue_infos);
-    let device = unsafe { instance.create_device(physical_device, &device_info, None) }?;
+    let device = unsafe {
+        instance
+            .instance
+            .create_device(physical_device, &device_info, None)
+    }?;
 
     let surface_capabilities = unsafe {
         surface_loader.get_physical_device_surface_capabilities(physical_device, surface)
@@ -296,7 +208,7 @@ fn main() -> Result<()> {
         .present_mode(present_modes[0])
         .clipped(true);
 
-    let swapchain_loader = Swapchain::new(&instance, &device);
+    let swapchain_loader = Swapchain::new(&instance.instance, &device);
     let swapchain = unsafe { swapchain_loader.create_swapchain(&swapchain_create_info, None)? };
 
     let pool_create_info = vk::CommandPoolCreateInfo::builder()
@@ -842,12 +754,6 @@ fn main() -> Result<()> {
         swapchain_loader.destroy_swapchain(swapchain, None);
         device.destroy_device(None);
         surface_loader.destroy_surface(surface, None);
-        if let Some(_dbg_loader) = _dbg_loader {
-            if let Some(_dbg_callbk) = _dbg_callbk {
-                _dbg_loader.destroy_debug_utils_messenger(_dbg_callbk, None);
-            }
-        }
-        instance.destroy_instance(None);
     }
 
     println!("End destroying");
