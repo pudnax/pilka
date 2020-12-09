a272ff406e918e1bf31ece7f0c69b1f9c9103647
Alex Komissarov <k.a.komissar@gmail.com>
2020-12-09 15:23:53 +0500

    Add TODO(#4)

--- libs/pilka_ash/src/lib.rs
+++ libs/pilka_ash/src/lib.rs
@@ -239,7 +239,7 @@ pub mod ash {
         }
     }
 
-    // TODO: Consider about Arc
+    // TODO(#4): Consider about Arc
     pub struct VkDevice {
         device: Arc<RawDevice>,
         physical_device: vk::PhysicalDevice,
@@ -903,6 +903,45 @@ pub mod ash {
 
                 self.device.destroy_command_pool(self.pool, None);
             }
+        }
+    }
+
+    pub struct VkShaderModule {
+        pub module: vk::ShaderModule,
+        device: Arc<Device>,
+    }
+
+    impl VkShaderModule {
+        pub fn new<P: AsRef<std::path::Path>>(
+            path: P,
+            shader_type: shaderc::ShaderKind,
+            compiler: &mut shaderc::Compiler,
+            device: Arc<Device>,
+        ) -> VkResult<Self> {
+            let shader_text = std::fs::read_to_string(&path).unwrap();
+            let shader_data = compiler
+                .compile_into_spirv(
+                    &shader_text,
+                    shader_type,
+                    path.as_ref().to_str().unwrap(),
+                    "main",
+                    None,
+                )
+                .unwrap();
+            let shader_data = shader_data.as_binary_u8();
+            let mut shader_data = std::io::Cursor::new(shader_data);
+            // TODO: Switch to wgou variant.
+            let shader_code = ash::util::read_spv(&mut shader_data).unwrap();
+            let shader_info = vk::ShaderModuleCreateInfo::builder().code(&shader_code);
+
+            let module = unsafe { device.create_shader_module(&shader_info, None) }?;
+            Ok(VkShaderModule { module, device })
+        }
+    }
+
+    impl Drop for VkShaderModule {
+        fn drop(&mut self) {
+            unsafe { self.device.destroy_shader_module(self.module, None) };
         }
     }
 }
