a4c6a33cf39b68597fd42e1176fc8221169c48b0
Alex Komissarov <k.a.komissar@gmail.com>
2021-01-02 18:52:15 +0500

    Clean warnings

--- libs/pilka_incremental/src/app.rs
+++ libs/pilka_incremental/src/app.rs
@@ -665,10 +665,6 @@ impl PilkaRender {
     }
 }
 
-unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
-    std::slice::from_raw_parts((p as *const T) as *const u8, std::mem::size_of::<T>())
-}
-
 impl Drop for PilkaRender {
     fn drop(&mut self) {
         unsafe {
