ba5fbeda0379c5e3ee5caf0d05073d803be1fee5
Alex Komissarov <k.a.komissar@gmail.com>
2021-01-03 21:27:09 +0500

    Shenanigans

--- libs/pilka_ash/src/utils.rs
+++ libs/pilka_ash/src/utils.rs
@@ -1,7 +1,6 @@
 use ash::vk;
 
 /// # Safety
-///
 /// Until you're using it on not ZST or DST it's fine
 pub unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
     std::slice::from_raw_parts((p as *const T) as *const _, std::mem::size_of::<T>())
