ffbcf105a78aae10b71b27586394609e7b3907b3
Alex Komissarov <k.a.komissar@gmail.com>
2021-02-24 19:28:40 +0500

    Update template shaders

--- src/default_shaders.rs
+++ src/default_shaders.rs
@@ -30,6 +30,10 @@ const FRAG_SHADER: &str = "
 layout(location = 0) in vec2 uv;
 layout(location = 0) out vec4 out_color;
 
+layout(set = 0, binding = 0) uniform sampler2D previous_frame;
+#define T(t) (texture(t, vec2(uv.x, -uv.y)))
+#define T_off(t,off) (texture(t, vec2(uv.x + off.x, -(uv.y + off.y))))
+
 layout(std430, push_constant) uniform PushConstant {
 	vec3 pos;
 	float time;
