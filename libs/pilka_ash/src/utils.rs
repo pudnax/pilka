86ecc93127e9999509f86453f9c40a7e33932d99
Alex Komissarov <k.a.komissar@gmail.com>
2021-03-06 15:23:10 +0500

    Move barrier, copy functions to the device module
    Former-commit-id: d17063f4ccbb33cbfb5f7b68651d108de4b0e71e
    Former-commit-id: 63015d68efbd968d19a3ca80719054300887d581

--- libs/pilka_ash/src/utils.rs
+++ libs/pilka_ash/src/utils.rs
@@ -1,5 +1,3 @@
-use crate::device::VkDevice;
-use ash::version::DeviceV1_0;
 use ash::vk;
 
 pub fn find_memory_type_index(
@@ -33,88 +31,6 @@ fn find_memorytype_index_f<F: Fn(vk::MemoryPropertyFlags, vk::MemoryPropertyFlag
         memory_type_bits >>= 1;
     }
     None
-}
-
-#[allow(clippy::clippy::too_many_arguments)]
-pub fn set_image_layout(
-    device: &VkDevice,
-    cmd_buffer: vk::CommandBuffer,
-    image: vk::Image,
-    old_layout: vk::ImageLayout,
-    new_layout: vk::ImageLayout,
-    subresource_range: vk::ImageSubresourceRange,
-    src_stage_mask: vk::PipelineStageFlags,
-    dst_stage_mask: vk::PipelineStageFlags,
-) {
-    let mut image_memory_barrier = vk::ImageMemoryBarrier::builder()
-        .old_layout(old_layout)
-        .new_layout(new_layout)
-        .image(image)
-        .subresource_range(subresource_range);
-
-    use vk::{AccessFlags, ImageLayout};
-    image_memory_barrier.src_access_mask = match old_layout {
-        ImageLayout::UNDEFINED => AccessFlags::empty(),
-        ImageLayout::PREINITIALIZED => AccessFlags::HOST_WRITE,
-        ImageLayout::COLOR_ATTACHMENT_OPTIMAL => AccessFlags::COLOR_ATTACHMENT_WRITE,
-        ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL => {
-            AccessFlags::DEPTH_STENCIL_ATTACHMENT_WRITE
-        }
-        ImageLayout::TRANSFER_SRC_OPTIMAL => AccessFlags::TRANSFER_READ,
-        ImageLayout::TRANSFER_DST_OPTIMAL => AccessFlags::TRANSFER_WRITE,
-        ImageLayout::SHADER_READ_ONLY_OPTIMAL => AccessFlags::SHADER_READ,
-        _ => AccessFlags::empty(),
-    };
-
-    image_memory_barrier.dst_access_mask = match new_layout {
-        ImageLayout::TRANSFER_DST_OPTIMAL => AccessFlags::TRANSFER_WRITE,
-        ImageLayout::TRANSFER_SRC_OPTIMAL => AccessFlags::TRANSFER_READ,
-        ImageLayout::COLOR_ATTACHMENT_OPTIMAL => AccessFlags::COLOR_ATTACHMENT_WRITE,
-        ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL => {
-            image_memory_barrier.dst_access_mask | AccessFlags::DEPTH_STENCIL_ATTACHMENT_WRITE
-        }
-        ImageLayout::SHADER_READ_ONLY_OPTIMAL => {
-            if image_memory_barrier.src_access_mask.is_empty() {
-                image_memory_barrier.src_access_mask =
-                    AccessFlags::HOST_WRITE | AccessFlags::TRANSFER_WRITE;
-            }
-            AccessFlags::SHADER_READ
-        }
-        _ => AccessFlags::empty(),
-    };
-
-    let image_barriers = [image_memory_barrier.build()];
-    unsafe {
-        device.cmd_pipeline_barrier(
-            cmd_buffer,
-            src_stage_mask,
-            dst_stage_mask,
-            vk::DependencyFlags::empty(),
-            &[],
-            &[],
-            &image_barriers,
-        );
-    }
-}
-
-pub fn set_image_layout_all_commands(
-    device: &VkDevice,
-    cmd_buffer: vk::CommandBuffer,
-    image: vk::Image,
-    old_layout: vk::ImageLayout,
-    new_layout: vk::ImageLayout,
-    subresource_range: vk::ImageSubresourceRange,
-) {
-    set_image_layout(
-        device,
-        cmd_buffer,
-        image,
-        old_layout,
-        new_layout,
-        subresource_range,
-        vk::PipelineStageFlags::ALL_COMMANDS,
-        vk::PipelineStageFlags::ALL_COMMANDS,
-    );
 }
 
 /// # Safety
