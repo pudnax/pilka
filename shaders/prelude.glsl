eb73a3af68cff4adb3558719a2f089763eb15814
Alex Komissarov <k.a.komissar@gmail.com>
2021-03-12 19:52:41 +0500

    Format shaders
    Former-commit-id: f00607a1339dd26278a94f1f035a09bb1e3127e6
    Former-commit-id: cdca7f17de7c72de571f7e6ed570c28c942311c1

--- shaders/prelude.glsl
+++ shaders/prelude.glsl
@@ -14,12 +14,14 @@ float worldSDF(vec3 rayPos);
 vec2 ray_march(vec3 rayPos, vec3 rayDir) {
     float dist = 0.0;
 
-    for(int i = 0; i < MAX_STEPS; i++) {
+    for (int i = 0; i < MAX_STEPS; i++) {
         vec3 pos = rayPos + (dist * rayDir);
         float posToScene = worldSDF(pos);
         dist += posToScene;
-        if(abs(posToScene) < HIT_DIST) return vec2(dist, i);
-        if(posToScene > MISS_DIST) break;
+        if (abs(posToScene) < HIT_DIST)
+            return vec2(dist, i);
+        if (posToScene > MISS_DIST)
+            break;
     }
 
     return vec2(-dist, MAX_STEPS);
@@ -31,18 +33,20 @@ float crossSDF(vec3 rayPos) {
     vec3 cornerToRay = ray - corner;
     float minComp = min(min(cornerToRay.x, cornerToRay.y), cornerToRay.z);
     float maxComp = max(max(cornerToRay.x, cornerToRay.y), cornerToRay.z);
-    float midComp = cornerToRay.x + cornerToRay.y + cornerToRay.z
-                                             - minComp - maxComp;
+    float midComp =
+        cornerToRay.x + cornerToRay.y + cornerToRay.z - minComp - maxComp;
     vec2 closestOutsidePoint = max(vec2(minComp, midComp), 0.0);
     vec2 closestInsidePoint = min(vec2(midComp, maxComp), 0.0);
-    return (midComp > 0.0) ? length(closestOutsidePoint) : -length(closestInsidePoint);
+    return (midComp > 0.0) ? length(closestOutsidePoint)
+                           : -length(closestInsidePoint);
 }
 
 float cubeSDF(vec3 rayPos) {
     const vec3 corner = vec3(HALF_WIDTH);
     vec3 ray = abs(rayPos);
     vec3 cornerToRay = ray - corner;
-    float cornerToRayMaxComponent = max(max(cornerToRay.x, cornerToRay.y), cornerToRay.z);
+    float cornerToRayMaxComponent =
+        max(max(cornerToRay.x, cornerToRay.y), cornerToRay.z);
     float distToInsideRay = min(cornerToRayMaxComponent, 0.0);
     vec3 closestToOusideRay = max(cornerToRay, 0.0);
     return length(closestToOusideRay) + distToInsideRay;
@@ -83,13 +87,13 @@ mat2 rotate(float angle) {
 }
 
 vec3 enlight(in vec3 at, vec3 normal, vec3 diffuse, vec3 l_color, vec3 l_pos) {
-  vec3 l_dir = l_pos - at;
-  return diffuse * l_color * max(0., dot(normal, normalize(l_dir))) /
-         dot(l_dir, l_dir);
+    vec3 l_dir = l_pos - at;
+    return diffuse * l_color * max(0., dot(normal, normalize(l_dir))) /
+           dot(l_dir, l_dir);
 }
 
 vec3 wnormal(in vec3 p) {
-  return normalize(vec3(worldSDF(p + EPS.yxx) - worldSDF(p - EPS.yxx),
-                        worldSDF(p + EPS.xyx) - worldSDF(p - EPS.xyx),
-                        worldSDF(p + EPS.xxy) - worldSDF(p - EPS.xxy)));
+    return normalize(vec3(worldSDF(p + EPS.yxx) - worldSDF(p - EPS.yxx),
+                          worldSDF(p + EPS.xyx) - worldSDF(p - EPS.xyx),
+                          worldSDF(p + EPS.xxy) - worldSDF(p - EPS.xxy)));
 }
--- shaders/shader.vert
+++ shaders/shader.vert
@@ -10,6 +10,6 @@ layout(std430, push_constant) uniform PushConstant {
 } pc;
 
 void main() {
-  out_uv = vec2((gl_VertexIndex << 1) & 2, gl_VertexIndex & 2);
-  gl_Position = vec4(out_uv * 2.0f + -1.0f, 0.0, 1.0);
+    out_uv = vec2((gl_VertexIndex << 1) & 2, gl_VertexIndex & 2);
+    gl_Position = vec4(out_uv * 2.0f + -1.0f, 0.0, 1.0);
 }
