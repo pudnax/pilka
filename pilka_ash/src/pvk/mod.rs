716c883ad3a3511a32aafe41812664ae733f5a40
Alex Komissarov <k.a.komissar@gmail.com>
2021-10-02 18:49:41 +0500

    Add support for switching between backends
    Former-commit-id: 30501baed4be9b40ad9bca66bfeb03af98a5de62 [formerly 3072c72aba71cb301f69729ed5cb06c2124fa1e6] [formerly 2403c3a76372208f8eb8a86e0babdbdede1af6bf]
    Former-commit-id: b833d14d6f6e23eef959c888ecfa3703bfdfebe9
    Former-commit-id: c66c0eb2223474067fc4835bb9821e97cfd1285a

--- pilka_ash/src/pvk/mod.rs
+++ pilka_ash/src/pvk/mod.rs
@@ -5,7 +5,6 @@ mod device;
 mod image;
 mod instance;
 mod renderpass_and_pipeline;
-mod shader_module;
 mod surface;
 mod swapchain;
 mod texture;
@@ -17,9 +16,7 @@ pub use instance::{VkInstance, VkQueue, VkQueues};
 pub use renderpass_and_pipeline::{
     Pipeline, PipelineDescriptor, VkComputePipeline, VkGraphicsPipeline, VkRenderPass,
 };
-pub use shader_module::{
-    create_shader_module, ShaderSet, VkShaderModule, SHADER_ENTRY_POINT, SHADER_PATH,
-};
+
 pub use surface::VkSurface;
 pub use swapchain::VkSwapchain;
 
